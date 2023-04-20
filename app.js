4f7cf72a3e72571bbd8538a0a1878f7784b83659
javiersaezgarcia <banbleac@gmail.com>
2023-04-20 18:57:09 +0200

    port issue

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 require('dotenv').config();
-const PORT = process.env.PORT | 8081;
+const PORT = process.env.PORT;
 
 
 // *******************    CONTROLLERS   ***************************
